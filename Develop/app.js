3c16773fd7274151d865227cece89c33b0e8e667
Alvy <floressuarezalvaro@gmail.com>
2020-10-28 21:25:47 -0700

    testing

--- Develop/app.js
+++ Develop/app.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+let todaysDate = document.getElementById('currentDay');
+
+function getApi() {
+    var requestUrl = 'https://momentjs.com/';
+    
+    fetch(requestUrl)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    // Use the console to examine the response
+    console.log("hi")
+    // TODO: Loop through the data and generate your HTML
+    for (let i = 0; i < data.length; i++) {
+        console.log("Hi there.")
+    //diplayDate.textContent = data[i].login;
+    //today.append("Hello Punk");
+    }
+    });
+
+    moment().format();
+
+}
